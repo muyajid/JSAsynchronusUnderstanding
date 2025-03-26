8ddfada69fbd6495c7114904bdc940da1f7e19c8
muyajid <kamuyazidkan@gmail.com>
2025-03-27 03:51:33 +0700

    fixing eror

--- code/asynawait.js
+++ code/asynawait.js
@@ -1,45 +1,47 @@
 function getApiUrl() {
-    return `https://dummyjson.com/comments/add`;
+    return `https://dummyjson.com/auth/login`;
 }
-function postComment(comment) {
+function authLogin(username, password) {
     const apiUrl = getApiUrl();
-    const promise = new Promise((resolve, reject) => {
-        
+    
+    return new Promise((resolve, reject) => {
         const ajax = new XMLHttpRequest();
 
         ajax.open('POST', apiUrl);
         ajax.setRequestHeader("Content-Type", "application/json");
-
         const body = JSON.stringify({
-            body: comment,
-            postId: 3,
-            userId: 2
+            username: username,
+            password: password
         });
 
         ajax.send(body);
-        ajax.onload = () => {
-
-            const serverRespon = ajax.responseText;
-            if (ajax.status === 200) {
-                resolve(serverRespon);
+        ajax.onload = (() => {
+            if(ajax.status === 200) {
+                resolve(ajax.responseText);
             } else {
-                reject(serverRespon);
+                reject(ajax.responseText);
             }
-        }
+        })
     })
+}
 
-    return promise;
-}
-async function implApi(comment) {
+async function implApi() {
     try {
-        const apiPromise = await postComment(comment);
-        console.log(apiPromise);
+
+        const inpUsername = document.getElementById('inpUsername');
+        const inpPassword = document.getElementById('inpPassword');
+        const imgProfile = document.getElementById('imgProfile');
+        const pUsername = document.getElementById('pUsername');
+        const apiPromise = await authLogin(inpUsername.value, inpPassword.value);
+
+        const serverRespon = JSON.parse(apiPromise);
+        console.log(serverRespon);
+
+        imgProfile.setAttribute("src", serverRespon.image);
+        pUsername.textContent = `Selamat Datang ${serverRespon.username}`;
     } catch (error) {
-        console.error(error);
+        console.error(`Terjadi Eror : ${error}`);
+    } finally {
+        console.info(`Develop By Yazid`);
     }
-}
-function btnPostComment() {
-    console.info("Develop By Yazid");
-    const inpComment = document.getElementById('inpComment');
-    implApi(inpComment.value);
 }
