6d12c47cf28ed504f5731921ac41319a8bf42608
muyajid <kamuyazidkan@gmail.com>
2025-04-13 14:30:16 +0700

    dokumentasi

--- comunicationpractice/03_praktek_Webworker.js
+++ comunicationpractice/03_praktek_Webworker.js
@@ -1,11 +1,15 @@
+// menangkap data dari client untuk di proses
 addEventListener("message", async function (event) {
+    // proses fetching dengan return promise
     try {
         const req = await fetch(event.data, {
             method: 'GET'
         });
         const res = await req.json();
+        // mengirim data hasil dari proses fetching 
         postMessage(res);
     } catch (err) {
+        // mengirim status eror bila terjadi eror
         postMessage(`Terjadi Eror : ${err}`);
     }
 })
