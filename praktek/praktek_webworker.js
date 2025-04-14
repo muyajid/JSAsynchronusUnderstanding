b4aabd645af940f62a5961962617f14ed25c30fb
muyajid <kamuyazidkan@gmail.com>
2025-04-14 11:22:36 +0700

    postMessage

--- praktek/praktek_webworker.js
+++ praktek/praktek_webworker.js
@@ -14,6 +14,6 @@ addEventListener("message", async function (event) {
             postMessage(`Terjadi Eror : ${error}`);
           }
     } else {
-        console.log("Hello");
+      postMessage("Tidak Ada Perintah Req Ke Server");
     }
 });
